d23e0ef5dcb5c91dafa2db70ff125f9ce4b9e24c
Lucas Andrade <lucasaraujo@si.fiponline.edu.br>
2020-04-05 22:57:59 -0300

    submetendo exercício ws

---
+++ index.js
@@ -0,0 +1,16 @@
+const soap = require('soap')
+
+const url = 'https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl'
+
+soap.createClient(url, (err, client) => {
+    if(err){
+        console.log(err)
+    } else {
+        console.log('ok')
+        client.consultaCEP({
+           cep: '58703095' 
+        }, (err, res) => {
+            console.log(res)
+        })
+    }    
+})
